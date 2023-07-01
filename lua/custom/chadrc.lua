40d8dca9e8016658894b5718c9da63edfc5159cb
Stephen Morgan <steve@rebelops.io>
2023-07-01 16:00:04 -0400

    feat: transparency and copilot
    Signed-off-by: Stephen Morgan <steve@rebelops.io>

--- lua/custom/chadrc.lua
+++ lua/custom/chadrc.lua
@@ -1,6 +1,7 @@
 ---@type ChadrcConfig 
- local M = {}
- M.ui = {theme = 'catppuccin'}
- M.plugins = "custom.plugins"
- M.mappings = require "custom.mappings"
- return M
+local M = {}
+M.ui = { theme = 'catppuccin', transparency = true, }
+M.plugins = "custom.plugins"
+M.mappings = require "custom.mappings"
+return M
+
