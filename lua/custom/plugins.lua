ab836006e35e615eb326f83e9cd6f1eadcfd70b2
Stephen Morgan <steve@rebelops.io>
2023-07-02 15:27:15 -0400

    feat: additional plugins
    Signed-off-by: Stephen Morgan <steve@rebelops.io>

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -26,7 +26,6 @@ local plugins = {
     opts = {
       ensure_installed = {
         "gopls",
-        "tsserver",
         "black",
         "debugpy",
         "mypy",
@@ -133,6 +132,20 @@ local plugins = {
           copilot_cmp._on_insert_enter({})
         end
       end)
+    end,
+  },
+  {
+    "dreamsofcode-io/ChatGPT.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim"
+    },
+    config = function()
+      require("chatgpt").setup({
+        api_key_cmd = "op read op://private/OpenAI/credential --no-newline"
+      })
     end,
   },
 }
