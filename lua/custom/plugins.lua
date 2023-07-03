258bd2bc0bb1714fe65dde1c5d02aab54fbb2b0e
Stephen Morgan <steve@rebelops.io>
2023-07-02 22:53:04 -0400

    feat: add lazygit plugin

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -148,5 +148,13 @@ local plugins = {
       })
     end,
   },
+  {
+    "kdheepak/lazygit.nvim",
+    -- optional for floating window border decoration
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    lazy = false,
+  },
 }
 return plugins
